854aa0513376939adef41dd9305e777ee0c7bed3
Saumya Bhatnagar <saumya.bhatnagar.sb@gmail.com>
2018-05-05 21:14:21 +0530

    script to send email with intruder picture to owner

---
+++ sendNotify.py
@@ -0,0 +1,43 @@
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEBase import MIMEBase
+from email.MIMEText import MIMEText
+from email.Utils import COMMASPACE, formatdate
+from email import Encoders
+import os
+
+USERNAME = "username@gmail.com"
+PASSWORD = "password"
+
+def sendMail(to, subject, text, files=[]):
+    assert type(to)==list
+    assert type(files)==list
+
+    msg = MIMEMultipart()
+    msg['From'] = USERNAME
+    msg['To'] = COMMASPACE.join(to)
+    msg['Date'] = formatdate(localtime=True)
+    msg['Subject'] = subject
+
+    msg.attach( MIMEText(text) )
+
+    for file in files:
+        part = MIMEBase('application', "octet-stream")
+        part.set_payload( open(file,"rb").read() )
+        Encoders.encode_base64(part)
+        part.add_header('Content-Disposition', 'attachment; filename="%s"'
+                       % os.path.basename(file))
+        msg.attach(part)
+
+	server = smtplib.SMTP('smtp.gmail.com:587')
+	server.ehlo_or_helo_if_needed()
+	server.starttls()
+	server.ehlo_or_helo_if_needed()
+	server.login(USERNAME,PASSWORD)
+	server.sendmail(USERNAME, to, msg.as_string())
+	server.quit()
+
+sendMail( ["owner-email@domain.com"],
+        "Intruder notification",
+        "Someone is trying to enter through door, picture attached",
+        ["/home/pi/intruder.jpg"] )
